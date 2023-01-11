651fc6a587d50714b8b39d98517c1412b5d0dbb7
jeonghyeon Hwang <hijihilin@gmail.com>
2023-01-11 23:19:31 +0900

    [BUILD] Add Initial Project

---
+++ CustomVideoPlayer/CustomVideoPlayer/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CustomVideoPlayer
+//
+//  Created by 황정현 on 2023/01/11.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
