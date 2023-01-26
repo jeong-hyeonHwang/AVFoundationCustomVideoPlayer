845abc7d879dfaf4be64c11b29362dcc6f13fef1
jeonghyeon Hwang <hijihilin@gmail.com>
2023-01-26 23:20:00 +0900

    [FEAT] Add Video Player

--- CustomVideoPlayer/CustomVideoPlayer/VideoPlayerView.swift
+++ CustomVideoPlayer/CustomVideoPlayer/VideoPlayerView.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import AVFoundation
 
 enum ControlStatus {
     case exist
@@ -33,21 +34,49 @@ final class VideoPlayerView: UIView {
     
     var videoPlayerControlView: VideoPlayerControlView = VideoPlayerControlView()
     
+    
+    var player: AVPlayer? {
+        get {
+            return playerLayer.player
+        }
+        set {
+            playerLayer.player = newValue
+        }
+    }
+        
+    var playerLayer: AVPlayerLayer {
+        return layer as! AVPlayerLayer
+    }
+    
+    private var playerItemContext = 0
+
+    private var playerItem: AVPlayerItem?
+    
     override init(frame: CGRect  =  CGRect()) {
         super.init(frame: frame)
 
         setUpLayout()
         componentConfigure()
+        playVideo()
     }
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
     
+    deinit {
+        playerItem?.removeObserver(self, forKeyPath: #keyPath(AVPlayerItem.status))
+        print("deinit of PlayerView")
+    }
+    
+    override class var layerClass: AnyClass {
+        return AVPlayerLayer.self
+    }
+    
     private func setUpLayout() {
         
         // TEST
-        self.backgroundColor = .yellow
+        self.backgroundColor = .black
         
         let controlViewHeight = UIScreen.main.bounds.width / 12
         addSubview(videoPlayerControlView)
@@ -68,8 +97,80 @@ final class VideoPlayerView: UIView {
         self.addGestureRecognizer(touchGesture)
     }
     
+    private func setUpAsset(with url: URL, completion: ((_ asset: AVAsset) -> Void)?) {
+        let asset = AVAsset(url: url)
+        
+        Task {
+            let isPlayable = try await asset.load(.isPlayable)
+            
+            if isPlayable {
+                completion?(asset)
+            } else {
+                print("Non-playable")
+            }
+        }
+    }
+    
+    // https://medium.com/@tarasuzy00/build-video-player-in-ios-i-avplayer-43cd1060dbdc
+    private func setUpPlayerItem(with asset: AVAsset) {
+        playerItem = AVPlayerItem(asset: asset)
+        playerItem?.addObserver(self, forKeyPath: #keyPath(AVPlayerItem.status), options: [.old, .new], context: &playerItemContext)
+            
+        DispatchQueue.main.async { [weak self] in
+            self?.player = AVPlayer(playerItem: self?.playerItem!)
+        }
+    }
+        
+    override func observeValue(forKeyPath keyPath: String?, of object: Any?, change: [NSKeyValueChangeKey : Any]?, context: UnsafeMutableRawPointer?) {
+        guard context == &playerItemContext else {
+            super.observeValue(forKeyPath: keyPath, of: object, change: change, context: context)
+            return
+        }
+            
+        if keyPath == #keyPath(AVPlayerItem.status) {
+            let status: AVPlayerItem.Status
+            if let statusNumber = change?[.newKey] as? NSNumber {
+                status = AVPlayerItem.Status(rawValue: statusNumber.intValue)!
+            } else {
+                status = .unknown
+            }
+            
+            switch status {
+            case .readyToPlay:
+                print(".readyToPlay")
+                player?.play()
+            case .failed:
+                print(".failed")
+            case .unknown:
+                print(".unknown")
+            @unknown default:
+                print("@unknown default")
+            }
+        }
+    }
+    
+    func play(with url: URL) {
+        setUpAsset(with: url) { [weak self] (asset: AVAsset) in
+            self?.setUpPlayerItem(with: asset)
+        }
+    }
+    
+    func playVideo() {
+        guard let url = URL(string: videoURL) else { return }
+        self.play(with: url)
+    }
+    
+    func pause() {
+        playerLayer.player?.pause()
+    }
+    
+    func replay() {
+        playerLayer.player?.play()
+    }
+    
     @objc private func touchVideoPlayerScreen() {
         testControlStatus.changeControlStatus(view: videoPlayerControlView)
     }
-                                            
+    
+    
 }
